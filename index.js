07398b9642f1084e901d939704752d3ab457d1af
Ace LaCabe <150067821+AceChianni@users.noreply.github.com>
2024-12-20 19:34:28 +0000

    create update folder, remove old code

--- index.js
+++ index.js
@@ -1,29 +1,46 @@
-// index.js
+// // index.js
+
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 require('dotenv').config();
+
 const productRoutes = require('./routes/products');
 const authRoutes = require('./routes/auth');
-const auth = require('./middleware/auth');
 const userRoutes = require('./routes/users');
+const auth = require('./middleware/auth');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Middleware to parse JSON bodies
-app.use(express.json());
+// Middleware
+app.use(express.json()); 
+
+// Serve static files
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
 
-// Import Routes
-app.use('/products', productRoutes); 
-app.use('/auth', authRoutes); 
-app.use('/users', auth, userRoutes);
+// Default Route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Coffee Shop');
+});
+
+
+// Routes
+app.use('/auth', authRoutes); // Authentication routes
+app.use('/users', auth, userRoutes); // User routes with authentication
+app.use('/products', auth, productRoutes); // Product routes protected with authentication
+
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Protect product routes with authentication middleware
 app.use('/products', auth, productRoutes);
